3a01d9ed0fd7f825091fd0eb7055b49a1fd66926
JAYD3V <W3Dojo@Gmail.com>
2022-07-26 23:44:44 -0700

    feat(Color Class): :sparkles: Combined HSV class with RGB class to create a single class called Color (or "Color Class"/"Color API"). The class needs work but it can do some conversions now

--- src/api/hsv.ts
+++ src/api/hsv.ts
@@ -1,73 +1,135 @@
-// TYPES
-type RGB = { red:number, grn:number, blu:number };
-type HSV = { hue:number, sat:number, val:number };
-
-// GLOBALS
-const { round: round, max:largest, min:smallest } = Math;
+const { round, abs } = Math;
 
 
-export default function rgbToHsv (rgb: RGB): HSV{
-   const { red, grn, blu } = rgb;
-   return {
-      hue : getRgbHue(red, grn, blu),
-      sat : getRgbSat(red, grn, blu),
-      val : getRgbBrightness(red, grn, blu)
-   };
-}
+// Color object types
+export type ColorObj_HSV = { hue: number; sat: number; val: number; };
+export type ColorObj_RGB = { red: number; grn: number; blu: number; };
+export type ColorObj = ColorObj_HSV | ColorObj_RGB;
 
+// Color array types
+export type ColorArr_HSV = [number, number, number];
+export type ColorArr_RGB = [number, number, number];
+export type ColorArr = ColorArr_HSV | ColorArr_RGB;
+
+// Color format types
+export type HSV_Format = ColorObj_HSV | ColorArr_HSV;
+export type RGB_Format = ColorObj_RGB | ColorArr_RGB;
 
 
 
 /**
- * Returns the range of the 3 RGB color values. This number is
- * commonly referred to as "Delta" when expressed inside of an
- * algorithm.
+ * #### COLOR CLASS
+ * The Color class contains an entire interface for working with colors in many different environments. It can be used for printing color in a terminal environment, or for converting color values from HSV to RGB. There are too many features to list in this snippet, for a full list please see the project documentation. The official documentation source can be found in several place, the README.md file in the J-Color directory, on the Npm J-Color Package page, or in the [J-Color Repository @ Https://GitHub.com/JAYD3V/J-Color](Https://GitHub.com/JAYD3V/J-Color)
  *
- * @example
-      const delta = findRgbRange(100, 20, 35)
-      console.log(delta) // Logs: 80
- * _____________________________________________________________ */
-function lengthOfRgbRange (red:number, grn:number, blu:number){
-   return largest(red, grn, blu) - smallest(red, grn, blu);
-}
+ * @param ColorName Name of the color being abstracted.
+ * @param hsv Set of values used to define an individually unique color */
+export default class Color{
+   hsv: ColorObj_HSV;
+   name: string;
 
-
-
-
-function getRgbHue (red:number, grn:number, blu:number){
-   const delta = lengthOfRgbRange(red, grn, blu);
-   const max = largest(red, grn, blu);
-
-   let hue = 0;
-
-   if (delta != 0){
-      hue = red === max
-         ? ((grn - blu) / delta) % 6 : grn === max
-               ? (blu - red) / delta + 2 : (red - grn) / delta + 4;
+   constructor (colorName: string, hsv: HSV_Format){
+      this.hsv = (Array.isArray(hsv)) ? Color.arrayToObject(hsv) : hsv;
+      this.name = colorName;
    }
 
-   hue = round(hue * 60);
-   hue += (hue < 0) ? 360 : 0;
+   toRgb (){
+      let { hue: H, sat: S, val: V } = this.hsv;
 
-   return hue;
+      if (H > 0 && H < 360){
+         throw new RangeError('A color\'s hue must be between 0 and 360');
+      }
+      if (S < 0 || S > 100){
+         throw new RangeError('Saturation must be between 0 and 100');
+      }
+      if (V < 0 || V > 100){
+         throw new RangeError('Value must be between 0 & 100');
+      }
+
+      H /= 60; S /= 100; V /= 100;
+
+      const C = V * S,
+            X = C * (1 - abs(H % 2 - 1)),
+            m = V - C;
+
+      const matchComponents = (): ColorObj_RGB => {
+         switch (H){
+            case 0: return { red: C, grn: X, blu: 0 };
+            case 1: return { red: X, grn: C, blu: 0 };
+            case 2: return { red: 0, grn: C, blu: X };
+            case 3: return { red: 0, grn: X, blu: C };
+            case 4: return { red: X, grn: 0, blu: C };
+            default: return { red: C, grn: 0, blu: X }; // H has 2 be 5
+         }
+      };
+
+
+      const RGB = matchComponents();
+
+      let key: keyof ColorObj_RGB;
+
+      for (key in RGB) RGB[key] = round(255 * (RGB[key] + m));
+
+      return RGB;
+   }
+   /*
+
+
+   *==============================================*
+                   STATIC MEMBERS
+   *==============================================*/
+   static arrayToObject (hsv: ColorArr_HSV){
+      return { hue: hsv[0], sat: hsv[1], val: hsv[2] };
+   }
 }
 
 
 
 
-function getRgbBrightness (red:number, grn:number, blu:number){
-   // Find the color w/ greatest value, the convert it to a percentage
-   return round(largest(red, grn, blu) / 255 * 100);
-}
 
+/** ### HSV to RGB Color-model Coversion Function */
+export function hsvToRgb (hsv: ColorObj_HSV | ColorArr_HSV){
+   // If arg is arr convert to type: ColorObj_HSV
+   const HSV = Array.isArray(hsv)
+      ? { hue: hsv[0], sat: hsv[1], val: hsv[2] } : hsv;
 
+   let { hue: H, sat: S, val: V } = HSV;
 
+   if (H > 0 && H < 360){
+      throw new RangeError('A color\'s hue must be between 0 and 360');
+   }
 
-function getRgbSat (red:number, grn:number, blu:number){
-   const delta = lengthOfRgbRange(red, grn, blu);
-   const max = largest(red, grn, blu);
+   if (S < 0 || S > 100){
+      throw new RangeError('Saturation must be between 0 and 100');
+   }
 
-   // find the percentage that the range is of the greatest color
-   // then convert it to a 1-100 percentage value.
-   return (delta <= 0) ? 0 : round(delta / max * 100);
+   if (V < 0 || V > 100){
+      throw new RangeError('Value must be between 0 & 100');
+   }
+
+   H /= 60;
+   S /= 100;
+   V /= 100;
+
+   const C = V * S,
+         X = C * (1 - abs(H % 2 - 1)),
+         m = V - C;
+
+   const matchComponents = (): ColorObj_RGB => {
+      switch (H){
+         case 0: return { red: C, grn: X, blu: 0 };
+         case 1: return { red: X, grn: C, blu: 0 };
+         case 2: return { red: 0, grn: C, blu: X };
+         case 3: return { red: 0, grn: X, blu: C };
+         case 4: return { red: X, grn: 0, blu: C };
+         default: return { red: C, grn: 0, blu: X }; // H has 2 be 5
+      }
+   };
+
+   const RGB = matchComponents();
+
+   let key: keyof ColorObj_RGB;
+
+   for (key in RGB) RGB[key] = round(255 * (RGB[key] + m));
+
+   return RGB;
 }
