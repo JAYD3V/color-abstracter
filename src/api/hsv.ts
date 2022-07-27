ebbddd042db6748a5a2ebfdc748b4e7dd7835d0e
JAYD3V <W3Dojo@Gmail.com>
2022-07-27 00:27:02 -0700

    refactor(Color Class): :art: Formatted Code in src/api/hsv.ts

--- src/api/hsv.ts
+++ src/api/hsv.ts
@@ -1,6 +1,5 @@
 const { round, abs } = Math;
 
-
 // Color object types
 export type ColorObj_HSV = { hue: number; sat: number; val: number; };
 export type ColorObj_RGB = { red: number; grn: number; blu: number; };
@@ -17,23 +16,36 @@ export type RGB_Format = ColorObj_RGB | ColorArr_RGB;
 
 
 
-/**
- * #### COLOR CLASS
- * The Color class contains an entire interface for working with colors in many different environments. It can be used for printing color in a terminal environment, or for converting color values from HSV to RGB. There are too many features to list in this snippet, for a full list please see the project documentation. The official documentation source can be found in several place, the README.md file in the J-Color directory, on the Npm J-Color Package page, or in the [J-Color Repository @ Https://GitHub.com/JAYD3V/J-Color](Https://GitHub.com/JAYD3V/J-Color)
- *
- * @param ColorName Name of the color being abstracted.
- * @param hsv Set of values used to define an individually unique color */
-export default class Color{
-   hsv: ColorObj_HSV;
-   name: string;
 
-   constructor (colorName: string, hsv: HSV_Format){
-      this.hsv = (Array.isArray(hsv)) ? Color.arrayToObject(hsv) : hsv;
+
+
+export default class Color{
+   // PRIVATE FIELDS
+   readonly #_HSV: ColorObj_HSV;
+   readonly name: string;
+
+
+   #_arrayToObject = (hsv: ColorArr_HSV) => ({
+      hue: hsv[0], sat: hsv[1], val: hsv[2]
+   });
+
+
+   /**
+    * #### COLOR CLASS
+    * The Color class contains an entire interface for working with colors in many different environments. It can be used for printing color in a terminal environment, or for converting color values from HSV to RGB. There are too many features to list in this snippet, for a full list please see the project documentation. The official documentation source can be found in several place, the README.md file in the J-Color directory, on the Npm J-Color Package page, or in the [J-Color Repository @ Https://GitHub.com/JAYD3V/J-Color](Https://GitHub.com/JAYD3V/J-Color)
+    *
+    * @param colorName Name of the color being abstracted.
+    * @param colorsValues Set of values used to define an individually unique color * */
+   constructor (colorName: string, colorsValues: HSV_Format){
       this.name = colorName;
+      this.#_HSV = (Array.isArray(colorsValues))
+         ? this.#_arrayToObject(colorsValues)
+         : colorsValues;
    }
 
+
    toRgb (){
-      let { hue: H, sat: S, val: V } = this.hsv;
+      let { hue: H, sat: S, val: V } = this.#_HSV;
 
       if (H > 0 && H < 360){
          throw new RangeError('A color\'s hue must be between 0 and 360');
@@ -48,8 +60,8 @@ export default class Color{
       H /= 60; S /= 100; V /= 100;
 
       const C = V * S,
-            X = C * (1 - abs(H % 2 - 1)),
-            m = V - C;
+            m = V - C,
+            X = C * (1 - abs(H % 2 - 1));
 
       const matchComponents = (): ColorObj_RGB => {
          switch (H){
@@ -58,78 +70,21 @@ export default class Color{
             case 2: return { red: 0, grn: C, blu: X };
             case 3: return { red: 0, grn: X, blu: C };
             case 4: return { red: X, grn: 0, blu: C };
-            default: return { red: C, grn: 0, blu: X }; // H has 2 be 5
+            // Default == 'case 5:' but satisfies TSC
+            default: return { red: C, grn: 0, blu: X };
          }
       };
-
 
       const RGB = matchComponents();
 
       let key: keyof ColorObj_RGB;
 
-      for (key in RGB) RGB[key] = round(255 * (RGB[key] + m));
+      for (key in RGB){
+         RGB[key] = round(255 * (RGB[key] + m));
+      }
 
       return RGB;
    }
-   /*
 
 
-   *==============================================*
-                   STATIC MEMBERS
-   *==============================================*/
-   static arrayToObject (hsv: ColorArr_HSV){
-      return { hue: hsv[0], sat: hsv[1], val: hsv[2] };
-   }
-}
-
-
-
-
-
-/** ### HSV to RGB Color-model Coversion Function */
-export function hsvToRgb (hsv: ColorObj_HSV | ColorArr_HSV){
-   // If arg is arr convert to type: ColorObj_HSV
-   const HSV = Array.isArray(hsv)
-      ? { hue: hsv[0], sat: hsv[1], val: hsv[2] } : hsv;
-
-   let { hue: H, sat: S, val: V } = HSV;
-
-   if (H > 0 && H < 360){
-      throw new RangeError('A color\'s hue must be between 0 and 360');
-   }
-
-   if (S < 0 || S > 100){
-      throw new RangeError('Saturation must be between 0 and 100');
-   }
-
-   if (V < 0 || V > 100){
-      throw new RangeError('Value must be between 0 & 100');
-   }
-
-   H /= 60;
-   S /= 100;
-   V /= 100;
-
-   const C = V * S,
-         X = C * (1 - abs(H % 2 - 1)),
-         m = V - C;
-
-   const matchComponents = (): ColorObj_RGB => {
-      switch (H){
-         case 0: return { red: C, grn: X, blu: 0 };
-         case 1: return { red: X, grn: C, blu: 0 };
-         case 2: return { red: 0, grn: C, blu: X };
-         case 3: return { red: 0, grn: X, blu: C };
-         case 4: return { red: X, grn: 0, blu: C };
-         default: return { red: C, grn: 0, blu: X }; // H has 2 be 5
-      }
-   };
-
-   const RGB = matchComponents();
-
-   let key: keyof ColorObj_RGB;
-
-   for (key in RGB) RGB[key] = round(255 * (RGB[key] + m));
-
-   return RGB;
 }
