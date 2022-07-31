b1b8f5116f36ada0034ead7fa503a844186e32dd
JAYD3V <W3Dojo@Gmail.com>
2022-07-31 10:42:06 -0700

    refactor(API: HSV Class): :bug: Rebuilt the algorithm from the ground up
    The previous version I wrote was buggy, unsupprisingly, the second version I wrote works very well in comparison.

--- src/api/hsv.ts
+++ src/api/hsv.ts
@@ -1,89 +1,92 @@
-const { round, abs } = Math;
-
-// Color object types
-export type HsvObj = { hue: number; sat: number; val: number; };
-export type RgbObj = { red: number; grn: number; blu: number; };
-export type ColorObject = HsvObj | RgbObj;
-
-// Color array types
 export type HsvArr = [number, number, number];
 export type RgbArr = [number, number, number];
-export type ColorArr = HsvArr | RgbArr;
-
-// Color format types
-export type HsvArg = HsvObj | HsvArr;
-export type RgbArg = RgbObj | RgbArr;
-export type ColorArg = RgbArg | HsvArg;
+export type RgbObj = { R:number, G:number, B:number };
+export type HsvObj = { H:number, S:number, V:number };
 
 
+
+const { abs, trunc, round } = Math;
 
 
 /**
- *
- * **HSV Class**
- * The HSV Class is used to create abstract colors in HSV format. It contains methods for preforming the color-model conversions **_"HSV to HexColor"_** & **_"HSV to RGB"_**. An array containing `[hue, saturation, value]` can be used to create a new HSV class, or an object containing `{ hue:num, sat:num, val:num }` can be used. The HSV Class works closely with the other abstract classes RGB & HexColor. The J Color class is meant to work in an environment where multiple color models are present.
- * */
+ * HSV Class - Abstraction of a color thats defined by the "HSV Color Model". This class is to be used as is, if working with HSV, or to convert from and to HSV. the toRGB method converts the color (in hsv format) to an object formated as the more common RGB color model. See README.md for full details, and a fill list of methods & class-members. */
 export default class HSV{
-    // PRIVATE FIELDS
-    readonly #_HSV: HsvObj;
-    readonly name: string;
+    name;
+    H; S; V;
 
-    #_arrayToObject = (hsv: HsvArr) => ({
-        hue: hsv[0], sat: hsv[1], val: hsv[2]
-    });
+    constructor(name: string, hsv:  [number, number, number]){
+        this.name = name;
 
-
-    /**
-    * @param colorName Name of the color being abstracted.
-    * @param hsv Set of values used to define an individually unique color * */
-    constructor(colorName: string, hsv: HsvObj | HsvArr){
-        this.name = colorName;
-        this.#_HSV = (Array.isArray(hsv))
-            ? this.#_arrayToObject(hsv)
-            : hsv;
+        this.H = this.#coercedValidation_H(hsv[0]);
+        this.S = this.#coercedValidation_S(hsv[1]);
+        this.V = this.#coercedValidation_V(hsv[2]);
     }
 
-    toRgb(){
-        let { hue: H, sat: S, val: V } = this.#_HSV;
 
-        if (H < 0 || H > 360){
-            throw new RangeError('A color\'s hue must be between 0 and 360');
-        }
+    #coercedValidation_H = (arg: number) => {
+        if (arg > 360) return 360;
+        if (arg < 0) return 0;
 
-        if (S < 0 || S > 100){
-            throw new RangeError('Saturation must be between 0 and 100');
-        }
+        return arg;
+    };
 
-        if (V < 0 || V > 100){
-            throw new RangeError('Value must be between 0 & 100');
-        }
 
-        H /= 60; S /= 100; V /= 100;
+    #coercedValidation_S = (arg: number) => {
+        if (arg > 100) return 100;
+        if (arg < 0) return 0;
 
-        const C = V * S,
-              m = V - C,
-              X = C * (1 - abs(H % 2 - 1));
+        return arg;
+    };
 
-        const matchComponents = (): RgbObj => {
-            switch (H){
-                case 0: return { red: C, grn: X, blu: 0 };
-                case 1: return { red: X, grn: C, blu: 0 };
-                case 2: return { red: 0, grn: C, blu: X };
-                case 3: return { red: 0, grn: X, blu: C };
-                case 4: return { red: X, grn: 0, blu: C };
-                    // Default == 'case 5:' but satisfies TSC
-                default: return { red: C, grn: 0, blu: X };
+
+    #coercedValidation_V = this.#coercedValidation_S;
+
+
+
+    toRGB(): RgbArr{
+        const H = this.H / 60;
+        const S = this.S / 100;
+        const V = this.V / 100;
+
+        const C = V * S;
+
+        const switcheroo = (H: number, C:number): RgbArr => {
+            const X = C * (1 - abs(H % 2 - 1));
+
+            switch (trunc(H)){
+                case 0: return [C, X, 0];
+                case 1: return [X, C, 0];
+                case 2: return [0, C, X];
+                case 3: return [0, X, C];
+                case 4: return [X, 0, C];
+
+                default: return [C, 0, X];
             }
         };
 
-        const RGB = matchComponents();
+        const m = (V - C);
+        const rgb = switcheroo(H, C);
 
-        let key: keyof RgbObj;
+        rgb.forEach((item, i, rgb) => {
+            rgb[i] = round((item + m) * 255);
+        });
 
-        for (key in RGB){
-            RGB[key] = round(255 * (RGB[key] + m));
-        }
-
-        return RGB;
+        return rgb;
     }
 }
+
+//             RED   GRN   BLU      HUE  SAT  VAL
+// ||-------------------------||-----------------||
+// || VIOLET:  122|   54|  225||    264|  76|  88||
+// ||   CYAN:   37|  244|  250||    182|  85|  98||
+// ||   LIME:  142|  227|   45||     88|  80|  89||
+// || ORANGE:  250|  168|   37||     37|  85|  98||
+// ||CRIMSON:  240|   36|  123||    334|  85|  94||
+// ||-------------------------||-----------------||
+
+
+const rgb = new HSV('Pseudo Color 4 Testing', [264, 76, 88]).toRGB();
+
+console.log('Printing Result: ');
+
+console.log(rgb);
