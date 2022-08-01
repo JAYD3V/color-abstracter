2fa910200f112e3cf5eb9b3ea99597c89a81e202
JAYD3V <W3Dojo@Gmail.com>
2022-08-01 08:28:46 -0700

    chore: :truck: -- MOVED TEST DIRECTORY

--- src/api/color.ts
+++ src/api/color.ts
@@ -1,7 +1,25 @@
 class Color{
     RGB = { red: 0, grn: 0, blu: 0 };
-
+    transparency: boolean;
     constructor(hexStr: string){
-        parseInt(hexStr);
+        const len = hexStr.length;
+
+        if (hexStr.at(0) !== '#'){
+            throw new Error('HexStr arg must start with a number-sign -> #');
+        }
+
+        if (len != 3 && len != 4 && len != 6 && len != 8){
+            console.log('');
+        }
+
+        switch (hexStr.length){
+            case 3:
+            case 6:
+            case 4:
+            case 8:
+            default:
+                throw new Error('HexColor arg has an invalid amount of digits');
+        }
+        // parseInt(hexStr, 10);
     }
 }
